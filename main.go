f20f370607a23385b66c097a25fb8955c4d5b71d
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-07-13 02:30:36 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
-import "log"
+import (
+	"log"
+	"strconv"
+	"os"
+)
 
 func main() {
-	fast := `
-version: '2'
+	text := `version: '2'
 
 services:`
 
-	log.Println(fast)
+	max := 10
+
+	key := 50010
+
+	for i := 1; i <= max; i ++{
+		orport := strconv.Itoa(key + i)
+		dirport := strconv.Itoa(key + i + 5000)
+		ctrport := strconv.Itoa(key + i - 10000)
+		appendText := `
+  tor` + strconv.Itoa(i) + `:
+    build: './torDocker'
+    ports:
+      - '` + orport + `:` + orport + `'
+      - '` + dirport + `:` + dirport + `'
+    environment:
+      ORPORT: '` + orport + `'
+      DIRPORT: '` + dirport + `'
+      CTRPORT: '` + ctrport + `'
+      NAME: 'Azunyan1111Relay` + strconv.Itoa(i) + `'
+    volumes:
+      - ./data/tor` + strconv.Itoa(i) + `:/root/.tor/
+`
+		text = text + appendText
+	}
+
+	file, err := os.Create(`./docker-compose.yml`)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	file.Write(([]byte)(text))
+
+	log.Println(text)
 }
--- torDocker/startup.sh
+++ torDocker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname Azunyan1111TorRelay
+tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname $NAME
 
