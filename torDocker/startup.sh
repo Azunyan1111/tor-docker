6fb49470f3a8a59d0db141bd00ff4ae1083e2b7d
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-07-13 02:40:42 +0900

    add family

--- torDocker/startup.sh
+++ torDocker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname $NAME
+tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname $NAME -NodeFamily "F4366FD5FB4EBEDE8E73F4E04A08DCB8451A6FE9"
 
