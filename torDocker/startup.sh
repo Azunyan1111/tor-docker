69685ccd3001ff64c40193aa3095c482020494dd
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-07-17 01:10:50 +0900

    add family

--- torDocker/startup.sh
+++ torDocker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sleep (expr (date -j -f "%Y-%m-%d %H:%M:%S" "2018-7-17 01:05:00" "+%s") - (date +%s))
-tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname $NAME -NodeFamily "F4366FD5FB4EBEDE8E73F4E04A08DCB8451A6FE9"
+tor -socksport 0 -orport $ORPORT -dirport $DIRPORT -controlport $CTRPORT -exitrelay 0 -exitpolicy "reject *:*" -contactinfo s15a1302@bene.fit.ac.jp -nickname $NAME -NodeFamily "0691545ABEAF91A04DFC33B615C1AEACB32FF527"
